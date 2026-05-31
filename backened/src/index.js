f62e1231b6257a3c3d102507d4806f56be891093
vermahoney <vishusunar7@gmail.com>
2026-05-31 12:33:26 +0530

    fix cors

--- backened/src/index.js
+++ backened/src/index.js
@@ -17,15 +17,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin:[
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://realtimechatapplication.vercel.app",
-      "https://realtimechatapplication-b906jwftfr-himanshus-projects-0725c532.vercel.app",
-    ],
+    origin: true,
+    
     credentials: true,
   })
-);
+); 
 
 // routes
 app.use("/api/auth", authRoutes);
