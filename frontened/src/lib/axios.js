a97c2441e87f9f2d3c267a3c1feeb11b10f537c9
vermahoney <vishusunar7@gmail.com>
2026-05-31 15:17:51 +0530

    fix backened url typo

--- frontened/src/lib/axios.js
+++ frontened/src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "https://realtimechatapplication.onrender.com/api",
+      : "https://realtimechatappliaction.onrender.com/api", // ✅ appliaction
 
   withCredentials: true,
 });
