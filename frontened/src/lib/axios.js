6a66c28de8c0a8b5e5c1c4953be9fe6474edb1e6
vermahoney <vishusunar7@gmail.com>
2026-05-31 11:38:23 +0530

    fix production api url

--- frontened/src/lib/axios.js
+++ frontened/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://realtimechatapplication.onrender.com/api",
+
   withCredentials: true,
 });
